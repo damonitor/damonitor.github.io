f15adcdd9769b0049748714d4c5464acf5362532
SeongJae Park <sjpark@amazon.com>
2020-05-22 12:29:04 +0200

    Cleanup index files
    Signed-off-by: SeongJae Park <sjpark@amazon.com>

--- _index_doc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for f in `find doc -maxdepth 3 -name 'index.html' | sort`
-do
-	echo "<a href=$f>$f</a><br>"
-done
--- _index_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for f in `find test -name '*.html' | sort`
-do
-	echo "<a href=$f>$f</a><br>"
-done
