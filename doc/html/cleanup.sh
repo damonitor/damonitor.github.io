03c708be6f247cea2cbb537e7afe915d0f47e117
SeongJae Park <sjpark@amazon.de>
2020-08-04 13:33:54 +0200

    Add cleanup script
    The kernel doc build output contains many unnecessary files.  This
    commit adds a cleanup script for removal of the files.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

---
+++ doc/html/cleanup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+for doc in *
+do
+	if [ ! -d $doc ]
+	then
+		continue
+	fi
+	cd $doc
+
+	for dir in *
+	do
+		if [ ! -d "$dir" ] || [ "$dir" = "vm" ] ||
+			[ "$dir" = "admin-guide" ] || [ "$dir" = "_static" ]
+		then
+			continue
+		fi
+
+		echo "$dir"
+		rm -fr "$dir"
+	done
+
+	cd ".doctrees"
+	for dir in *
+	do
+		if [ ! -d "$dir" ] || [ "$dir" = "vm" ] ||
+			[ "$dir" = "admin-guide" ] || [ "$dir" = "_static" ]
+		then
+			continue
+		fi
+
+		echo "$dir"
+		rm -fr "$dir"
+	done
+
+	cd ../..
+done
