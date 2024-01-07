6578386c75ac3f6dd4473f6289cd507efd40fc2d
sjpark <sjpark@amazon.com>
2024-01-07 12:29:53 -0800

    test/result/perf: Add its own index generator and index page
    Signed-off-by: sjpark <sjpark@amazon.com>

---
+++ test/result/perf/index.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+INDEX_FILE='index.html'
+
+echo "
+<pre>
+
+Performance Test Results
+========================
+
+Below are links to the past DAMON performance test results of damon/next tree
+snapshots.  The source code for the performanced test is <a href=https://github.com/awslabs/damon-tests>available</a>.
+
+</pre>
+" > $INDEX_FILE
+
+pushd after_merge
+results=$(find . -name 'index.html' | sort --reverse --version-sort)
+popd
+
+for f in $results
+do
+	echo "<a href=./after_merge/$f>$f</a><br>" >> $INDEX_FILE
+done
